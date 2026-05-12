fd57385253c9c202958eda2393db592b2e7df47f
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-05-12 16:46:56 -0600

    chore: actualizar configuración de la aplicación y ajustes en la clase principal

--- src/main/java/com/mynorsanchez/kinalapp/KinalRestApplication.java
+++ src/main/java/com/mynorsanchez/kinalapp/KinalRestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KinalRestApplication {
+public class  KinalRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KinalRestApplication.class, args);
